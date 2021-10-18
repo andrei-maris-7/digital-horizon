2937d5c74d39034cb403bf766e3de03e8d42f2ea
andrei.maris.7 <andreimaris90@gmail.com>
2021-10-18 11:42:12 +0300

    Fixed loaders

--- next.config.js
+++ next.config.js
@@ -1,21 +1,4 @@
-// const withPlugins = require('next-compose-plugins');
-// const optimizedImages = require('next-optimized-images');
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
 
-// module.exports = withPlugins([optimizedImages], { target: 'serverless' });
-
-module.exports = {
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.mdx/,
-      use: [
-        options.defaultLoaders.babel,
-        {
-          loader: "@mdx-js/loader",
-          options: pluginOptions.options,
-        },
-      ],
-    });
-
-    return config;
-  },
-};
+module.exports = withPlugins([optimizedImages], { target: "serverless" });
