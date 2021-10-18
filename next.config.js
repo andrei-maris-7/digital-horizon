810adb92d3a711ab523feb363286aa2c2504f6ea
andrei.maris.7 <andreimaris90@gmail.com>
2021-10-18 20:05:18 +0300

    added webpack 4

--- next.config.js
+++ next.config.js
@@ -4,11 +4,5 @@ const optimizedImages = require("next-optimized-images");
 // module.exports = withPlugins([optimizedImages], { target: "serverless" });
 
 module.exports = {
-  webpack5: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-    }
-    return config;
-  },
+  webpack5: false,
 };
