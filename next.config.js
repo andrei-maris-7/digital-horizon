1ce5546887864d8099dc8571aca630ce71b115e9
andrei.maris.7 <andreimaris90@gmail.com>
2021-10-19 10:13:49 +0300

    changed next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
 
-module.exports = withPlugins([optimizedImages], { target: "serverless" });
+module.exports = withPlugins([optimizedImages], {
+  target: "serverless",
+  images: {
+    disableStaticImages: true,
+  },
+});
+
+// import withPlugins from "next-compose-plugins";
+// import optimizedImages from "next-optimized-images";
