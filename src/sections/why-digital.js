4e839fd69370e73feaec0b0059c5b1ccc358dd02
andrei.maris.7 <andreimaris90@gmail.com>
2021-11-03 13:28:57 +0200

    Added react-modal-video types

--- src/sections/why-digital.js
+++ src/sections/why-digital.js
@@ -12,6 +12,7 @@ import {
 } from "theme-ui";
 import { keyframes } from "@emotion/core";
 import TextFeature from "components/text-feature";
+
 import ModalVideo from "react-modal-video";
 import { IoIosPlay } from "react-icons/io";
 
