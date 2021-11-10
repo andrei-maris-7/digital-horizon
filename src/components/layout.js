602a667f609826d6ecd4a9fb85aa23a102a13916
andrei.maris.7 <andreimaris90@gmail.com>
2021-11-10 23:55:04 +0200

    Added favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,11 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui';
-import React, { useState } from 'react';
-import Sticky from 'react-stickynode';
-import Header from './header/header';
-import Footer from './footer/footer';
+import { jsx } from "theme-ui";
+import React, { useState } from "react";
+import Sticky from "react-stickynode";
+import Header from "./header/header";
+import Footer from "./footer/footer";
+
+import Head from "next/head";
 
 export default function Layout({ children }) {
   const [isSticky, setIsSticky] = useState(false);
@@ -15,13 +17,16 @@ export default function Layout({ children }) {
       setIsSticky(false);
     }
   };
-  
+
   return (
     <React.Fragment>
+      <Head>
+        <link rel="shortcut icon" href="/techworld-single.png" />
+      </Head>
       <Sticky innerZ={1001} top={0} onStateChange={handleStateChange}>
-        <Header className={`${isSticky ? 'sticky' : 'unSticky'}`} />
+        <Header className={`${isSticky ? "sticky" : "unSticky"}`} />
       </Sticky>
-      <main id="content" sx={{ variant: 'layout.main', }} >
+      <main id="content" sx={{ variant: "layout.main" }}>
         {children}
       </main>
       <Footer />
